7d107bcba1990e804adaeb87ff665462ed9294e1
llx <535921901@qq.com>
2016-03-23 10:09:53 +0800

    Initial Commit

---
+++ shakeDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shakeDemo
+//
+//  Created by 卢良潇 on 16/3/23.
+//  Copyright © 2016年 卢良潇. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
